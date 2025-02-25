c4df72c0f850e54e43dcfda9f590cd530978b170
Michael-WIM <michael@wimtechnologies.com>
2025-02-25 10:08:05 +0200

    Change part size to 10

--- src/main/java/org/replicadb/manager/S3Manager.java
+++ src/main/java/org/replicadb/manager/S3Manager.java
@@ -267,7 +267,7 @@ public class S3Manager extends SqlManager {
                 .numStreams(numStreams)
                 .numUploadThreads(2)
                 .queueCapacity(2)
-                .partSize(20);
+                .partSize(10);
 
         final List<MultiPartOutputStream> streams = manager.getMultiPartOutputStreams();
         int processedRows = 0;
